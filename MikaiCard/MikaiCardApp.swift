a35b5c5c7fe902506d043663f84bb65286cb0bc2
Mikai <Mikaiklint@icloud.com>
2024-06-04 12:07:19 +0200

    Initial Commit

---
+++ MikaiCard/MikaiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MikaiCardApp.swift
+//  MikaiCard
+//
+//  Created by Makai Klintman on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MikaiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
